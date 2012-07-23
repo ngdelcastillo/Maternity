ba8aaedd0b4e95e7091f4aaac03b1995c020cb40
Noel del Castillo <ngdelcastillo@gmail.com>
2012-07-23 22:06:20 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maternity::Application.config.secret_token = '570aadfc047e8060c980a59c208f49b413215b0d981001cb247149e65ef56068bce47c5734fd887054b3234eeb048ea8276e2f9fadb33de0bda40ab53196ebae'
