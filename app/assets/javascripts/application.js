dff5d56fa7fdabc026a25374c069f240bdfbf55e
Noel del Castillo <ngdelcastillo@gmail.com>
2012-07-24 21:08:19 +0800

    made some twicks in the routes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.anythingslider
+//= require jquery.anythingslider.video.min
+//= require jquery.anythingslider.min
 //= require twitter/bootstrap
 //= require_tree 
